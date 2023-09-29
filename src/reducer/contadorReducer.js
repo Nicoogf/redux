4627a107a6f720ae93938ce0bec566e11497d3ad
Nicoogf <nicolasgfalabella@gmail.com>
2023-09-29 11:00:57 -0300

    Rexud reducers

--- src/reducer/contadorReducer.js
+++ src/reducer/contadorReducer.js
@@ -5,6 +5,41 @@ const initialState =  {
 }
 
 //Reducer  = Funcion pura
-export default function contadorReducer( state = initialState , accion ){
-    switch()
+export default function contadorReducer( state = initialState , action ){
+    switch( accion.type){
+        case "INCREMENTAR" :{
+            return {
+                ...state,
+                contador: state.contador + 1 
+            }
+        }
+        case "DECREMENTAR" :{
+            return{
+                ...state,
+                contador: state.contador - 1 
+            }
+        } 
+        case "INCREMENTAR_5" :{
+            return{
+                ...state ,
+                contador: state.contador + action.payload
+            }
+        }
+        case "DECREMENTAR_5" : {
+            return{
+                ...state ,
+                contador: state.contador - action.payload
+            }
+        }
+        case "RESET" : {
+            return {
+                ...state ,
+                contador : 0
+            }
+        }
+        default : 
+            return{
+                state
+            }
+    }
 }
